abf7d2d1cd682f1c731809bd6cbe508e27ad3405
James Wang <veriathon@gmail.com>
2017-05-24 00:43:11 -0700

    Adding /backup/ prefix

--- boltbackup.go
+++ boltbackup.go
@@ -3,10 +3,11 @@ package boltbackup
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"time"
 
-	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/boltdb/bolt"
 )
 
@@ -17,8 +18,25 @@ func Do(db *bolt.DB, s3Bucket *s3.S3, bucketname string) error {
 		_, err := tx.WriteTo(buffInMem)
 		backupdate := time.Now()
 		params := &s3.PutObjectInput{
+			Bucket: aws.String(bucketname),
+			Key: aws.String(fmt.Sprintf("/backup/%s",
+				backupdate.Format("2006_01_02_15-04-05"))),
+			Body:                 bytes.NewReader(b.Bytes()),
+			ACL:                  aws.String("authenticated-read"),
+			ServerSideEncryption: aws.String("AES256"),
+			Metadata: map[string]*string{
+				"bolt-backup": aws.String(backupdate.Format("2006/01/02/15:04:05")),
+			},
+		}
+		_, err = s3Bucket.PutObject(params)
+		if err != nil {
+			return err
+		}
+
+		// write latest
+		params = &s3.PutObjectInput{
 			Bucket:               aws.String(bucketname),
-			Key:                  aws.String(backupdate.Format("2006_01_02_15-04-05")),
+			Key:                  aws.String("/backup/latest"),
 			Body:                 bytes.NewReader(b.Bytes()),
 			ACL:                  aws.String("authenticated-read"),
 			ServerSideEncryption: aws.String("AES256"),
